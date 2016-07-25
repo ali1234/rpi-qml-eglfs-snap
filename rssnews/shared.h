16782096b51eb60f6707780873754615cacfdc18
Alistair Buxton <a.j.buxton@gmail.com>
2016-07-25 12:47:40 +0000

    Add shared.h and adjust include path.

--- rssnews/shared.h
+++ rssnews/shared.h
@@ -37,5 +37,35 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
-#include "shared.h"
-DECLARATIVE_EXAMPLE_MAIN(demos/rssnews/rssnews)
+#include <QDir>
+#include <QGuiApplication>
+#include <QQmlEngine>
+#include <QQmlFileSelector>
+#include <QQuickView> //Not using QQmlApplicationEngine because many examples don't have a Window{}
+#define DECLARATIVE_EXAMPLE_MAIN(NAME) int main(int argc, char* argv[]) \
+{\
+    QGuiApplication app(argc,argv);\
+    app.setOrganizationName("QtProject");\
+    app.setOrganizationDomain("qt-project.org");\
+    app.setApplicationName(QFileInfo(app.applicationFilePath()).baseName());\
+    QQuickView view;\
+    if (qgetenv("QT_QUICK_CORE_PROFILE").toInt()) {\
+        QSurfaceFormat f = view.format();\
+        f.setProfile(QSurfaceFormat::CoreProfile);\
+        f.setVersion(4, 4);\
+        view.setFormat(f);\
+    }\
+    view.connect(view.engine(), &QQmlEngine::quit, &app, &QCoreApplication::quit);\
+    new QQmlFileSelector(view.engine(), &view);\
+    view.setSource(QUrl("qrc:///" #NAME ".qml")); \
+    if (view.status() == QQuickView::Error)\
+        return -1;\
+    view.setResizeMode(QQuickView::SizeRootObjectToView);\
+    if (QGuiApplication::platformName() == QLatin1String("qnx") || \
+          QGuiApplication::platformName() == QLatin1String("eglfs")) {\
+        view.showFullScreen();\
+    } else {\
+        view.show();\
+    }\
+    return app.exec();\
+}
